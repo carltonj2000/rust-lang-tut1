5133e112f4efda6a6f5ae923bed2e0285af3e65d
Carlton Joseph <carlton.joseph@gmail.com>
2022-08-16 20:01:23 -0700

    error handling options

--- rlt09-errors/src/main.rs
+++ rlt09-errors/src/main.rs
@@ -1,23 +1,39 @@
-use std::fs::File;
-use std::io::ErrorKind;
+use std::fs::{self, File};
+use std::io::{self};
 
 fn main() {
     a();
-    let name = "./readme.txt";
-    let file = File::open(name);
-    let file = match file {
-        Ok(file) => file,
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => match File::create(name) {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating file: {:?}", e),
-            },
-            other_error => panic!("Problem opening file: {:?}", other_error),
-        },
-    };
-    print!("{:?}", file);
+    err_prop().expect("bad read");
+    err_panic();
 }
 
+fn err_panic() {
+    let name = "./readme.txt";
+    let file = File::open(name).expect("Failed to open file.");
+    println!("{:?}", file);
+}
+
+fn err_prop() -> Result<String, io::Error> {
+    fs::read_to_string("./src/main1.rs")
+
+    // simplified above
+    // let mut s = String::new();
+    // File::open("./src/main.rs")?.read_to_string(&mut s)?;
+    // Ok(s)
+
+    // let f = File::open("readme.txt");
+    // the ? above does the same as this code here
+    // let mut f = match f {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e),
+    // };
+
+    // same as ? and Ok(s) above
+    // match f.read_to_string(&mut s) {
+    //     Ok(_) => Ok(s),
+    //     Err(e) => Err(e),
+    // }
+}
 fn a() {
     b();
 }
